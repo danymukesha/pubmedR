5d7fd299c5e8b610143443218ec1a0bb0e2d3f8c
danymukesha <danymukesha@gmail.com>
2024-07-13 07:13:44 +0200

    rename the package

--- R/globals.R
+++ R/globals.R
@@ -2,6 +2,6 @@ DATABASE_LABEL = "PubMed"
 BASE_URL = "https://eutils.ncbi.nlm.nih.gov"
 MAX_ENTRIES_PER_PAGE = 1000
 
-utils::globalVariables("DATABASE_LABEL", "pubmedR", add = TRUE)
-utils::globalVariables("BASE_URL", "pubmedR", add = TRUE)
-utils::globalVariables("MAX_ENTRIES_PER_PAGE", "pubmedR", add = TRUE)
+utils::globalVariables("DATABASE_LABEL", "pubmedr", add = TRUE)
+utils::globalVariables("BASE_URL", "pubmedr", add = TRUE)
+utils::globalVariables("MAX_ENTRIES_PER_PAGE", "pubmedr", add = TRUE)
